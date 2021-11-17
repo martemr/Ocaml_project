d17ac6991155bb2ebbdd4f7e642e6c2796bd28a1
Martin <martin.emery@sfr.fr>
2021-11-17 11:12:59 +0100

    clone version 2

--- src/Tools.ml
+++ src/Tools.ml
@@ -2,7 +2,7 @@
 open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
-let clone_nodes (gr:'a graph) =
+let clone_nodes (gr:'a graph) = n_fold gr new_node empty_graph ;;
 
 let gmap gr f = assert false
 
