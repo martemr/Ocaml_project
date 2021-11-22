7ee8b6c157f86e8012c3032dd2fa5182bc7711e2
Martin <martin.emery@sfr.fr>
2021-11-22 12:17:49 +0100

    commentaires dfs

--- src/dfs.ml
+++ src/dfs.ml
@@ -1,3 +1,9 @@
+(* Faire attention au chemin saturé
+    Label du chemin entre a et b de type (x, y), si x=0, il n'y a pas de chemin de b vers a,
+    si x=y pas de chemin de a vers b.
+    Dans les autres cas, le chemin est dans les deux sens.
+ *)
+
 let rec dfs g from to = 
     let next_arc = List.hd out_arcs g from
 
